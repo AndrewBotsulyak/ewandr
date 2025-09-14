21f191158491ef568b2b8086d76a1a2f37fc41ac
andrew <q000000pa@gmail.com>
2025-09-14 14:51:34 +0300

    - github workflow actions;

--- apps/clients/client_products-e2e/playwright.config.ts
+++ apps/clients/client_products-e2e/playwright.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, devices } from '@playwright/test';
-import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
@@ -15,7 +14,7 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:4201';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  ...nxE2EPreset(__filename, { testDir: './src' }),
+  testDir: './src',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL,
