72fdda8537324c214f68940f493f9690ef34fdd7
andrew <q000000pa@gmail.com>
2025-09-14 13:28:53 +0300

    - github workflow actions;

--- apps/clients/client-shell/module-federation.config.ts
+++ apps/clients/client-shell/module-federation.config.ts
@@ -1,5 +1,5 @@
 import { ModuleFederationConfig } from '@nx/module-federation';
-import {mfSharedLibs, sharedFn, sharedKeys} from "@ewandr-workspace/core";
+import {sharedFn} from "../../../libs/core/src";
 
 const config: ModuleFederationConfig = {
   name: 'client-shell',
