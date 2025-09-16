f419eb4e1d5e06534274440e7049df9230f34093
andrew <q000000pa@gmail.com>
2025-09-16 16:07:50 +0300

    - added populated data to be-vendure

--- libs/client-shell/feature-navbar/src/lib/navbar/navbar.component.ts
+++ libs/client-shell/feature-navbar/src/lib/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonUI} from "@ewandr-workspace/ui-shared-lib";
 import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'lib-navbar',
-  imports: [CommonModule, MatButtonUI, RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
